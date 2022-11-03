46756c5542a71554a46212bdc36c8f30672d6479
Yuliia Stepkina <metulia@mail.ru>
2022-11-03 23:33:40 +0300

    added some changes

--- app/src/main/java/ui/CalculatorPresenter.java
+++ app/src/main/java/ui/CalculatorPresenter.java
@@ -7,17 +7,34 @@ public class CalculatorPresenter {
 
     private CalculatorView view;
     private Calculator calculator;
+    private double argOne;
+    private Double argTwo;
+    private Operator selectedOperator;
 
     public CalculatorPresenter(CalculatorView view, Calculator calculator) {
         this.view = view;
         this.calculator = calculator;
     }
 
-    public void onDigitPressed (int digit) {
+    public void onDigitPressed(int digit) {
+
+        if (argTwo == null) {
+            argOne = argOne * 10 + digit;
+            view.showResult(String.valueOf(argOne));
+        } else {
+            argTwo = argTwo * 10 + digit;
+            view.showResult(String.valueOf(argTwo));
+        }
 
     }
 
     public void onOperatorPressed(Operator operator) {
+        if (selectedOperator != null) {
+            argOne = calculator.calculate(argOne, argTwo, selectedOperator);
+            view.showResult(String.valueOf(argOne));
+        }
+        argTwo = 0.0;
+        selectedOperator = operator;
     }
 
     public void onDotPressed() {
